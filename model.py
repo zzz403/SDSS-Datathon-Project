5de3487cc3cbb8e03291ee59c8716ed5266511ca
zzz403 <august@Zhongzedebijibendiannao.local>
2025-03-02 00:38:47 -0500

    commit

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import xgboost as xgb
+import model as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, r2_score
 from sklearn.preprocessing import LabelEncoder
